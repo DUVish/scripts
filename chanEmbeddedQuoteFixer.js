c8888861f558da6fc528ebf74edba34487cf1509
DUVish <44996962+DUVish@users.noreply.github.com>
2018-11-22 08:44:54 -0800

    Can now tessellate inlined quotes in post

--- chanEmbeddedQuoteFixer.js
+++ chanEmbeddedQuoteFixer.js
@@ -2,7 +2,7 @@
 // @name         chan embedded quote fixer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Puts embedded quotes in end of post rather than beginning, can also click on checkboxes in posts to remove them, allows for expand all/collapse all via arrows in post header
+// @description  Puts embedded quotes in end of post rather than beginning, can also click on checkboxes in posts to remove them, can tessellate inlined quotes in post
 // @author       You
 // @match        http://boards.4chan.org/*/thread/*
 // @match        https://boards.4chan.org/*/thread/*
@@ -11,10 +11,6 @@
 //window.onload(function() {
     setTimeout(function() {
         let quotesList = Array.from(document.getElementsByClassName("quotelink")).filter(node => window.getComputedStyle(node).getPropertyValue("font-size") === "10.6667px");
-
-    //add preview inlined to class of embedded quote, wrap whole thing in mutation observer
-    //on click, add html, gray out link (add linkfade to classlist)
-    //on another click, find embedded post 1 layer deep, remove(), and then change link back to normal
 
         quotesList.forEach(node => {
           node.addEventListener("click", qEV);
@@ -31,9 +27,59 @@
 
         addRemoveCapability();
         addCollapseAndExpand();
-    }, 4000);
+        addTessellationQuotes();
+        addQuotedPostsContainer();
+    }, 3000);
 //});
 
+//tessellation feature - move element all the way to the right, try and move up - if interfering with getBoundingClientRect in two axes, then move left, then try and move up again - repeat until both up and left are exhausted
+  //repeat for all posts - tesselation would not just add display: flex and flex-wrap:wrap to thread container, but also margin/-margin to each individual post - can either add some caching capability by storing data on
+  //dome nodes themselves, or just store all of the HTML before, revert on option, and re-compute each time, possibly also add to barchive, also add for within posts themselves
+
+//image size slider - a slider appears when hovering or clicking over new span in image data row in post - has a slider, and position on slider determines the size of the full image via %
+
+function addTessellationQuotes() {
+  Array.from(document.getElementsByClassName("postInfo")).forEach(el => {
+    if (el.getElementsByClassName("tessellateQuotes").length === 0) {
+      let btn = el.querySelector(".postMenuBtn");
+      let tesSpan = document.createElement("span");
+      tesSpan.innerText = "[Tessellate]";
+      tesSpan.classList.add("tessellateQuotes");
+      tesSpan.style.paddingLeft = "6px";
+      tesSpan.style.paddingRight = "1px";
+      tesSpan.style.fontSize = "11px";
+      tesSpan.style.color = "rgb(46, 54, 144)";
+      el.insertBefore(tesSpan, btn);
+      tesSpan.removeEventListener("click", tessellateQuotes);
+      tesSpan.addEventListener("click", tessellateQuotes);
+    }
+  });
+}
+
+function tessellateQuotes(e) {
+  let tesSpan = e.target;
+  let quotesNode = e.target.parentNode.parentNode.querySelector(".quotedPostsContainer");
+  if (quotesNode.style.display !== "flex") {
+    quotesNode.style.display = "flex";
+    quotesNode.style.flexWrap = "wrap";
+    tesSpan.style.opacity = "0.55";
+  } else {
+    quotesNode.style.display = "";
+    quotesNode.style.flexWrap = "";
+    tesSpan.style.color = "rgb(46, 54, 144)";
+    tesSpan.style.opacity = "1.0";
+  }
+}
+
+function addQuotedPostsContainer() {
+  Array.from(document.getElementsByClassName("postMessage")).forEach(el => {
+    if (el.getElementsByClassName("quotedPostsContainer").length === 0) {
+      let container = document.createElement("span");
+      container.classList.add("quotedPostsContainer");
+      el.appendChild(container);
+    }
+  });
+}
 
 function addCollapseAndExpand() {
   Array.from(document.querySelectorAll(".post.reply")).forEach(postNode => {
@@ -114,14 +160,14 @@ function qEV(e, node=e.target) {
 
     //adding to parent post
     if (node.parentNode.parentNode.parentNode.parentNode.children[2].classList.contains("file")) {
-        node.parentNode.parentNode.parentNode.parentNode.children[3].appendChild(newPost);
+        Array.from(node.parentNode.parentNode.parentNode.parentNode.children[3].children).filter(el => el.classList.contains("quotedPostsContainer"))[0].appendChild(newPost);
         setTimeout(function() {node.parentNode.parentNode.parentNode.parentNode.children[3].children[0].remove();});
     } else {
-        node.parentNode.parentNode.parentNode.parentNode.children[2].appendChild(newPost);
+        Array.from(node.parentNode.parentNode.parentNode.parentNode.children[2].children).filter(el => el.classList.contains("quotedPostsContainer"))[0].appendChild(newPost);
         setTimeout(function() {node.parentNode.parentNode.parentNode.parentNode.children[2].children[0].remove();});
     }
     //console.log("node just added", newPost);
-    setTimeout(function(){newPost.style.display = "";}, 50);
+    setTimeout(function(){newPost.style.display = "";}, 10);
 
     //resetting event listeners
     resetQLEV();
@@ -129,8 +175,13 @@ function qEV(e, node=e.target) {
     //grayed out, second time clicking, removing node
     node.classList.remove("linkfade");
     let idx = node.parentNode.parentNode.parentNode.parentNode.children[2].classList.contains("file") ? 3 : 2;
-    Array.from(node.parentNode.parentNode.parentNode.parentNode.children[idx].children).forEach(el => {if (el.classList.contains("inlined")
-                                                                                                          && node.innerText.slice(2) === el.children[1].children[1].children[3].children[1].innerText) el.remove();});
+    Array.from(node.parentNode.parentNode.parentNode.parentNode.children[idx].children).forEach(innerNode => {
+      if (innerNode.classList.contains("quotedPostsContainer")) {
+        Array.from(innerNode.children).forEach(el => {
+          if (el.classList.contains("inlined") && node.innerText.slice(2) === el.children[1].children[1].children[3].children[1].innerText) el.remove();
+        });
+      }
+    });
   }
 }
 
@@ -151,6 +202,11 @@ function resetQLEV() {
   Array.from(document.getElementsByClassName("postInfo desktop")).forEach(el => el.children[0].removeEventListener("click", postRemove));
   addRemoveCapability();
   setTimeout(function() {addCollapseAndExpand();}, 100);
+  let tesQuoteSpans = Array.from(document.getElementsByClassName("tessellateQuotes"));
+  tesQuoteSpans.forEach(el => el.removeEventListener("click", tessellateQuotes));
+  tesQuoteSpans.forEach(el => el.addEventListener("click", tessellateQuotes));
+  addTessellationQuotes();
+  addQuotedPostsContainer();
 }
 
 let observer = new MutationObserver(resetQLEV);
