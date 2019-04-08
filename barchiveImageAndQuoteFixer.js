9eb0cdbac1d8c24f91ef9383e2aba58150acad81
DUVish <44996962+DUVish@users.noreply.github.com>
2019-04-08 15:29:50 -0700

    Added media size change and extra options hideable

--- barchiveImageAndQuoteFixer.js
+++ barchiveImageAndQuoteFixer.js
@@ -1,24 +1,28 @@
 // ==UserScript==
 // @name         barchive image and quote fixer
-// @namespace    http://tampermonkey.net/
+// @namespace    thebarchive
 // @version      0.1
-// @description  replaces thumbnails with full image or webm on click and inlines "quoted by" posts on click, can also remove posts, tessellate inlined posts, tessellate entire thread, and expand/collapse all 'quoted by' posts
-// @author       You
+// @description  replaces thumbnails with full image or webm on click, inlines "quoted by" posts on click, can also remove posts, tessellate inlined posts, tessellate entire thread, and expand/collapse all 'quoted by' posts, can color posts recursively downward or entire thread (helps keep track of deeply-embedded conversations), can hide all extra options on posts (to make them smaller/simpler) via toggle in OP, can change media size when expanded
+// @author       DUVish
 // @match        https://thebarchive.com/b/thread/*
 // @match        https://archived.moe/*/thread/*
 // @grant        none
 // ==/UserScript==
 
-Array.from($(".post_data")).forEach(el => {
-    el.innerHTML += `<span class="expandAllQuotes" style="font-size: 10px;position: relative;top: -2px;padding-left: 1px;color: #7fa5c6;opacity: 0.95;padding-right: 0px;" title="Expand all 'quoted by' posts">[↓]</span>`;
+$(".thread_image_link").on("click", function(e) {
+  e.preventDefault();
 });
 
 Array.from($(".post_data")).forEach(el => {
-    el.innerHTML += `<span class="collapseAllQuotes" style="font-size: 10px;position: relative;top: -2px;padding-left: 3px;color: #7fa5c6;opacity: 0.95;padding-right: 6px;" title="Collapse all 'quoted by' posts">[↑]</span>`;
+    el.innerHTML += `<span class="expandAllQuotes collapsible" style="font-size: 10px;position: relative;top: -2px;padding-left: 1px;color: #7fa5c6;opacity: 0.95;padding-right: 0px;" title="Expand all 'quoted by' posts">[↓]</span>`;
 });
 
 Array.from($(".post_data")).forEach(el => {
-    el.innerHTML += '<span class="tessellatePost" style="font-size: 10px;position: relative;top: -2px;padding-left: 4px;color: #7fa5c6;opacity: 0.95;padding-right: 13px;">[Tessellate]</span>';
+    el.innerHTML += `<span class="collapseAllQuotes collapsible" style="font-size: 10px;position: relative;top: -2px;padding-left: 3px;color: #7fa5c6;opacity: 0.95;padding-right: 6px;" title="Collapse all 'quoted by' posts">[↑]</span>`;
+});
+
+Array.from($(".post_data")).forEach(el => {
+    el.innerHTML += '<span class="tessellatePost collapsible" style="font-size: 10px;position: relative;top: -2px;padding-left: 4px;color: #7fa5c6;opacity: 0.95;padding-right: 13px;">[Tessellate]</span>';
 });
 
 document.querySelector(".post_data").innerHTML += '<span class="tessellateThread" style="font-size: 10px;position: relative;top: -2px;padding-left: 2px;color: #7fa5c6;opacity: 0.95;padding-right: 11px;">[Tessellate Thread]</span>';
@@ -27,19 +31,19 @@ document.querySelector(".post_data").innerHTML += '<span class="colorPosts" styl
 
 Array.from($(".post_data")).forEach((el, i) => {
     if (i === 0) return;
-    el.innerHTML += '<span class="colorPosts" style="font-size: 10px;position: relative;top: -2px;padding-left: 0px;color: #7fa5c6;opacity: 0.95;padding-right: 8px;right:2px" title="Color all posts in thread">[<span class="colorPostOn">Color Posts</span> | <span class="colorPostOff">Off</span>]</span>';
+    el.innerHTML += '<span class="colorPosts collapsible" style="font-size: 10px;position: relative;top: -2px;padding-left: 0px;color: #7fa5c6;opacity: 0.95;padding-right: 8px;right:2px" title="Color all posts in thread">[<span class="colorPostOn">Color Posts</span> | <span class="colorPostOff">Off</span>]</span>';
 });
 
 Array.from($(".post_data")).forEach(el => {
     el.children[8].children[0].style.paddingLeft = "0px";
     el.children[8].children[0].style.marginLeft = "-3px";
-    el.innerHTML += '<span class="postRemove" style="font-size: 9.5px;position: relative;top: -2px;padding-left: 3px;color: darkgrey;opacity: 0.45;">Remove</span>';
+    el.innerHTML += '<span class="postRemove collapsible" style="font-size: 9.5px;position: relative;top: -2px;padding-left: 3px;color: darkgrey;opacity: 0.45;">Remove</span>';
 });
 
 Array.from(document.querySelectorAll(".text")).forEach(el => el.innerHTML += '<span class="inlinedQuotesContainer"></span>');
 
 $(".colorOn").on("click", function(e) {
-  Array.from(document.getElementsByClassName("post_wrapper")).forEach(el => {el.style.backgroundColor = `rgb(${Math.floor(Math.random() * 16)}, ${Math.floor(Math.random() * 16)}, ${Math.floor(Math.random() * 16)})`;});
+  Array.from(document.getElementsByClassName("post_wrapper")).forEach(el => {el.style.backgroundColor = `rgb(${Math.random() < 0.5 ? 40 - Math.floor(Math.random() * 16) : 40 + Math.floor(Math.random() * 16)}, ${Math.random() < 0.5 ? 42 - Math.floor(Math.random() * 16) : 42 + Math.floor(Math.random() * 16)}, ${Math.random() < 0.5 ? 46 - Math.floor(Math.random() * 16) : 46 + Math.floor(Math.random() * 16)})`;});
 });
 
 $(".colorOff").on("click", function(e) {
@@ -47,7 +51,7 @@ $(".colorOff").on("click", function(e) {
 });
 
 $(".colorPostOn").on("click", function(e) {
-  Array.from(e.target.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".post_wrapper")).forEach(el => {el.style.backgroundColor = `rgb(${Math.floor(Math.random() * 16)}, ${Math.floor(Math.random() * 16)}, ${Math.floor(Math.random() * 16)})`;});
+  Array.from(e.target.parentNode.parentNode.parentNode.parentNode.querySelectorAll(".post_wrapper")).forEach(el => {el.style.backgroundColor = `rgb(${Math.random() < 0.5 ? 40 - Math.floor(Math.random() * 16) : 40 + Math.floor(Math.random() * 16)}, ${Math.random() < 0.5 ? 42 - Math.floor(Math.random() * 16) : 42 + Math.floor(Math.random() * 16)}, ${Math.random() < 0.5 ? 46 - Math.floor(Math.random() * 16) : 46 + Math.floor(Math.random() * 16)})`;});
 });
 
 $(".colorPostOff").on("click", function(e) {
@@ -197,37 +201,159 @@ function settingClickHandler(e, node) {
         node.attributes.removeNamedItem("width");
     } else {
         if (node.src.split("/")[5] === "thumb") {
-            if (node.parentNode.parentNode.parentNode.children[0].children[1].href.slice(-4) === "webm") {
+            let fileRow = node.parentNode.parentNode.parentNode.querySelector(".post_file");
+            let widthHeightArr = fileRow.children[2].innerText.split(", ")[1].trim().split("x");
+            if (fileRow.children[1].href.slice(-4) === "webm") {
                 let newVideoNode = document.createElement("video");
-                newVideoNode.src = node.parentNode.parentNode.parentNode.children[0].children[1].href;
+                newVideoNode.src = fileRow.children[1].href;
                 newVideoNode.controls = true;
                 newVideoNode.autoplay = true;
                 newVideoNode.loop = true;
-                node.parentNode.parentNode.parentNode.appendChild(newVideoNode);
+                newVideoNode.width = widthHeightArr[0];
+                node.parentNode.insertBefore(newVideoNode, node);
                 node.style.display = "none";
                 let newSpan = document.createElement("span");
                 newSpan.style.paddingLeft = "5px";
                 newSpan.style.color = "#81a2be";
                 newSpan.innerText = "[close webm]";
-                node.parentNode.parentNode.parentNode.children[0].appendChild(newSpan);
+                fileRow.appendChild(newSpan);
+                let sizeSpan = document.createElement("span");
+                let increaseSize = document.createElement("span");
+                increaseSize.classList.add("videoIncreaseSize");
+                let decreaseSize = document.createElement("span");
+                decreaseSize.classList.add("videoDecreaseSize");
+                let increaseSizeMore = document.createElement("span");
+                increaseSizeMore.classList.add("videoIncreaseSizeMore");
+                increaseSizeMore.innerHTML = `(<span style="font-size: 17px; position: relative; top: 2px">++</span>)`;
+                let decreaseSizeMore = document.createElement("span");
+                decreaseSizeMore.classList.add("videoDecreaseSizeMore");
+                decreaseSizeMore.innerHTML = `(<span style="font-size: 24px; position: relative; top: 3.5px">--</span>)`;
+                decreaseSizeMore.style.paddingRight = "4px";
+                sizeSpan.classList.add("sizeSpan");
+                increaseSize.innerText = "(++)";
+                increaseSize.style.padding = "0px 4px";
+                decreaseSize.innerText = "(--)";
+                decreaseSize.style.padding = "0px 4px";
+                sizeSpan.style.paddingLeft = "5px";
+                sizeSpan.style.color = "#81a2be";
+                let resetSpan = document.createElement("span");
+                resetSpan.innerText = "Reset";
+                resetSpan.classList.add("resetMediaSize");
+                resetSpan.style.paddingLeft = "4px";
+                resetSpan.style.position = "relative";
+                resetSpan.style.top = "1px";
+                sizeSpan.innerHTML = `[Size - ${decreaseSize.outerHTML} ${decreaseSizeMore.outerHTML} | ${increaseSize.outerHTML} ${increaseSizeMore.outerHTML} | ${resetSpan.outerHTML} ]`;
+                fileRow.appendChild(sizeSpan);
+                newVideoNode.addEventListener("click", function(e) {
+                  if (e.target.paused) e.target.play();
+                  else e.target.pause();
+                }, false);
                 newSpan.addEventListener("click", function(e) {
                   newVideoNode.remove();
                   node.style.display = "";
                   newSpan.remove();
+                  sizeSpan.remove();
+                });
+                fileRow.querySelector(".resetMediaSize").addEventListener("click", function(e) {
+                  newVideoNode.width = widthHeightArr[0];
+                });
+                fileRow.querySelector(".videoIncreaseSize").addEventListener("click", function(e) {
+                  let numWidth = Number(newVideoNode.width);
+                  newVideoNode.width = `${(numWidth + 10)}`;
+                });
+                fileRow.querySelector(".videoDecreaseSize").addEventListener("click", function(e) {
+                  let numWidth = Number(newVideoNode.width);
+                  newVideoNode.width = numWidth > 10 ? `${(numWidth - 10)}` : "0";
+                });
+                fileRow.querySelector(".videoIncreaseSizeMore").addEventListener("click", function(e) {
+                  let numWidth = Number(newVideoNode.width);
+                  newVideoNode.width = `${(numWidth + 100)}`;
+                });
+                fileRow.querySelector(".videoDecreaseSizeMore").addEventListener("click", function(e) {
+                  let numWidth = Number(newVideoNode.width);
+                  newVideoNode.width = numWidth > 100 ? `${(numWidth - 100)}` : "0";
                 });
                 return;
             }
-            let widthHeightArr = node.parentNode.parentNode.parentNode.children[0].children[2].innerText.split(", ")[1].split("x");
-            let fileExt = node.parentNode.parentNode.parentNode.children[0].children[1].href.split(".")[2];
-            node.src = node.src.replace("thumb", "image").replace("s.jpg", "." + fileExt);
+            let fileExt = fileRow.children[1].href.split(".")[2];
+            node.dataset.originalSrc = node.src;
+            node.src = node.parentNode.href;
             node.style.width = widthHeightArr[0] + "px";
             node.style.height = widthHeightArr[1] + "px";
+            let sizeSpan = document.createElement("span");
+            sizeSpan.classList.add("mediaChangeSize");
+            let increaseSize = document.createElement("span");
+            increaseSize.classList.add("mediaIncreaseSize");
+            increaseSize.style.padding = "0px 4px";
+            let decreaseSize = document.createElement("span");
+            decreaseSize.classList.add("mediaDecreaseSize");
+            let increaseSizeMore = document.createElement("span");
+            increaseSizeMore.classList.add("mediaIncreaseSizeMore");
+            increaseSizeMore.innerHTML = `(<span style="font-size: 17px; position: relative; top: 2px">++</span>)`;
+            let decreaseSizeMore = document.createElement("span");
+            decreaseSizeMore.classList.add("mediaDecreaseSizeMore");
+            decreaseSizeMore.innerHTML = `(<span style="font-size: 24px; position: relative; top: 3.5px">--</span>)`;
+            decreaseSizeMore.style.padding = "0px 4px";
+            decreaseSize.style.paddingLeft = "4px";
+            increaseSize.innerText = "(++)";
+            decreaseSize.innerText = "(--)";
+            let resetSpan = document.createElement("span");
+            resetSpan.innerText = "Reset";
+            resetSpan.classList.add("resetMediaSize");
+            resetSpan.style.paddingLeft = "4px";
+            resetSpan.style.position = "relative";
+            resetSpan.style.top = "1px";
+            sizeSpan.innerHTML = `[Size - ${decreaseSize.outerHTML} ${decreaseSizeMore.outerHTML} | ${increaseSize.outerHTML} ${increaseSizeMore.outerHTML} | ${resetSpan.outerHTML} ]`;
+            sizeSpan.style.paddingLeft = "5px";
+            sizeSpan.style.color = "#81a2be";
+            fileRow.appendChild(sizeSpan);
+            fileRow.querySelector(".resetMediaSize").addEventListener("click", function(e) {
+              node.style.width = widthHeightArr[0] + "px";
+              node.style.height = widthHeightArr[1] + "px";
+            });
+            fileRow.querySelector(".mediaIncreaseSize").addEventListener("click", function(e) {
+              let numWidth = Number(node.style.width.match(/(\d+)px/)[1]);
+              let numHeight = Number(node.style.height.match(/(\d+)px/)[1]);
+              node.style.width = `${numWidth + 10}px`;
+              node.style.height = `${numHeight + 10}px`;
+            });
+            fileRow.querySelector(".mediaDecreaseSize").addEventListener("click", function(e) {
+              let numWidth = Number(node.style.width.match(/(\d+)px/)[1]);
+              node.style.width = numWidth > 10 ? `${numWidth - 10}px` : "0px";
+              let numHeight = Number(node.style.height.match(/(\d+)px/)[1]);
+              node.style.height = numHeight > 10 ? `${numHeight - 10}px` : "0px";
+            });
+            fileRow.querySelector(".mediaIncreaseSizeMore").addEventListener("click", function(e) {
+              let numWidth = Number(node.style.width.match(/(\d+)px/)[1]);
+              let numHeight = Number(node.style.height.match(/(\d+)px/)[1]);
+              node.style.width = `${numWidth + 100}px`;
+              node.style.height = `${numHeight + 100}px`;
+            });
+            fileRow.querySelector(".mediaDecreaseSizeMore").addEventListener("click", function(e) {
+              let numWidth = Number(node.style.width.match(/(\d+)px/)[1]);
+              node.style.width = numWidth > 100 ? `${numWidth - 100}px` : "0px";
+              let numHeight = Number(node.style.height.match(/(\d+)px/)[1]);
+              node.style.height = numHeight > 100 ? `${numHeight - 100}px` : "0px";
+            });
         } else {
-            node.src = node.parentNode.href || node.src.replace(/(\d+)(\.)(\w+)/, "$1s$2jpg").replace("image", "thumb");
+            node.src = node.dataset.originalSrc;
             node.style.width = "";
             node.style.height = "";
+            node.parentNode.parentNode.parentNode.querySelector(".mediaChangeSize").remove();
         }
     }
 }
 
 Array.from(document.querySelectorAll(".backlink.op")).forEach(el => el.innerText += "  (OP)");
+
+document.querySelector(".post_data").innerHTML += '<span class="removeExtras" style="font-size: 10px;position: relative;top: -2px;padding-left: 8px;color: #7fa5c6;opacity: 0.95;padding-right: 11px;" title="Remove extra options from all posts">[Remove Extras]</span>';
+
+document.querySelector(".removeExtras").addEventListener("click", function(e) {
+  if (e.target.style.opacity === "0.95") {
+    e.target.style.opacity = "0.55";
+    Array.from(document.querySelectorAll(".collapsible")).forEach(el => el.style.display = "none");
+  } else {
+    e.target.style.opacity = "0.95";
+    Array.from(document.querySelectorAll(".collapsible")).forEach(el => el.style.display = "unset");
+  }
+});
